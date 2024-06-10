5cb2abfae6a0ff66ae157ea03272346574e20af4
EliasAdonnay <gamalielmch@gmail.com>
2024-06-10 12:13:17 -0600

    delete add 02

--- plot_phasor_cosine_02.m
+++ plot_phasor_cosine_02.m
@@ -1,38 +1,50 @@
+%% %%%%%%% Limpiar
 clear all, 
 close all, 
 clc
+
+%% %%%%%%% cargar señal
 [audio,fs] = audioread("audiowav.wav"); 
 audio=audio(44100: 110250,1);
 audio=audio-mean(audio);
-%%%%%%%%%%%%% Parametros para la transformada 
-T = 1/fs;             % Periodo de muestreo   
-L=length(audio);         % Longitud de la señal
+
+%% %%%%%%%%%%% Parametros para la transformada 
+T = 1/fs;              % Periodo de muestreo   
+L=length(audio);       % Longitud de la señal
 t = (0:L-1)*T;         % Vector del tiempo
 n = 2^nextpow2(L);     % Puntos de la tranformada 
 f = fs*(0:(n/2))/n;    % Vector de frecuencias
-%%%%%%%%%%%%% Transformada de Fourier señal uno
+
+%% %%%%%%%%%%% Transformada de Fourier
 audioF = fft(audio,n);        % Transformada
-UNO_mag = abs(audioF/L); 
-UNO_angle = angle(audioF/L); 
-%%%%%%%%%%%%%%%%%%
-p=audioF(4).*exp(1i*2*pi*f(4).*t);
+audio_mag = abs(audioF/L);      % Magnitud
+audio_angle = angle(audioF/L);  % Phase
+
+
+
+
+%% %%% Fasor, Real,Imaginario, Magnitud y fase
+harmonic=4;
+p=audioF(harmonic).*exp(-1i*2*pi*f(harmonic).*t);
 p_real=real(p);
 p_imag=imag(p);
-mag=sqrt(p_real.^2+p_imag.^2);
-angle=(atan(p_imag./p_real))*2;
-plot(angle)
-angle=unwrap(angle,0.9)/2;
+[angle,mag]=cart2pol(p_real,p_imag);
+
+%% %%% Gráficas identidad de Euler fasores/sinusoides
 figure, 
 ax1=subplot(1,2,1);
 hold on,
-xlim([-10,10])
-ylim([-10,10])
+xlim([-13,13])
+ylim([-13,13])
 ax2=subplot(1,2,2);
 hold on,
-ylim([-10,10])
+ylim([-13,13])
 xlim([0,t(end)])
-recupera=UNO_mag(4)*L.*cos(2*pi*f(4).*t+UNO_angle(4));
-for i=1:80:length(t)
+set(gcf ,'units','normalized')
+set(gcf ,'position',[0.0702    0.5057    0.5958    0.3962])
+recupera=audio_mag(harmonic)*L.*cos(2*pi*f(harmonic).*t-audio_angle(harmonic));
+
+for i=1:150:length(t)
 subplot(1,2,1)
 plot(p_imag(i),p_real(i),'.r');
 [xa1,ya1] = ds2nfu(ax1,p_imag(i),p_real(i));

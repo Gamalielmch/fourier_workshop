fffe2c6269353a9eeb0eb1dc0eddd1e03da2aa7e
EliasAdonnay <gamalielmch@gmail.com>
2024-06-10 14:17:10 -0600

    ipdat

--- RT_method_03.m
+++ RT_method_03.m
@@ -1,11 +1,17 @@
-clear all
-close all
+%% %%%%%%% Limpiar
+clear all, 
+close all, 
+clc
+
+%% %%%%%%% cargar imagen y binarizar imagen
 I=imread('mdck.png');
 bw=imbinarize(I(:,:,2),0.4);
 bw = bwmorph(bw,'majority');
 bw = bwmorph(bw,'majority');
 bw = imfill(bw,'holes');
 imshow(bw)
+
+%% %%%%%%% Método Radius-Theta Centroide, perimetro 
 stats=regionprops(bw,'Area','PixelIdxList','Centroid'); 
 [~,ind]=max([stats.Area]);
 %ind=1;
@@ -15,24 +21,24 @@ Cen=stats(ind).Centroid;
 bws= bwboundaries(bw);
 figure, hold on, axis ij
 plot(Cen(1),Cen(2),'bs')
-rho=zeros(1,length(bws{1}));
-theta=zeros(1,length(bws{1}));
-for i=1:length(bws{1})
+rho=zeros(1,length(bws{1})-1);
+theta=zeros(1,length(bws{1})-1);
+for i=1:length(bws{1})-1
 plot(bws{1}(i,2),bws{1}(i,1),'r.')
 h=plot([Cen(1) bws{1}(i,2)], [Cen(2) bws{1}(i,1)]);
 difx=Cen(1)-bws{1}(i,2);
 dify=Cen(2)-bws{1}(i,1);
-rho(i)=sqrt(difx^2+dify^2);
-theta(i)=2*(atan(dify/difx));
+[theta(i),rho(i)]=cart2pol(difx,dify);
 drawnow
 delete(h)
 end
-theta=unwrap(theta,0.9)/2;
-figure, polar(theta,rho)
+
+%% %%%%%%%Inter/extrapolation, perimeter
+figure, subplot(1,2,1),polar(theta,rho)
 np = 256; 
-theta_i=linspace(0,2*pi,np);
+theta_i=linspace(-pi,pi,np);
 rho_i=interp1(theta,rho,theta_i);
-figure, plot(theta,rho), hold on
+subplot(1,2,2), plot(theta,rho), hold on
 plot(theta_i,rho_i,'r--')
 
 
